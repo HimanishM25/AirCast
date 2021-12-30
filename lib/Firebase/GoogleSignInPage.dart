8e3e35b6c1802dde0d74d01016848f28c3543a49
Himanish Mandrekar <mandrekarh25@gmail.com>
2021-12-30 22:25:56 +0530

    add ui for get started and sign in page

--- lib/Firebase/GoogleSignInPage.dart
+++ lib/Firebase/GoogleSignInPage.dart
@@ -14,16 +14,79 @@ class _GSignInPageState extends State<GSignInPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: ElevatedButton(onPressed: () {
-          final provider = Provider.of<GoogleSignInProvider>(context, listen:false);
-          provider.googleLogin();
-          },
-        child: Text('Sign In with Google'),
 
+      body:
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                TextButton(
 
-        ),
-      ),
+                  onPressed: (){
+                    final provider = Provider.of<GoogleSignInProvider>(context, listen:false);
+                    provider.googleLogin();
+                  },
+
+                  child: Container(
+                    alignment: Alignment.center,
+                    height: 36,
+                    // Constructor Call #2
+                    child: Text(
+                      'Sign In with Google',
+                      style: TextStyle(
+                          fontSize: 16,
+                          // Constructor Call #3
+                          color: Colors.white,
+                          fontWeight: FontWeight.w700),
+                    ),
+                  ),
+
+                  ////////// Styling //////////
+                  style: ButtonStyle(
+                      splashFactory: NoSplash.splashFactory,
+                      // Constructor Call #4
+                      backgroundColor: MaterialStateProperty.all(Color(0xff363250)),
+                      shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15),
+                      ))),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+    TextButton(
+
+    onPressed: (){print('pressed button -> create an account');},
+
+    child: Container(
+    alignment: Alignment.center,
+    height: 36,
+    // Constructor Call #2
+    child: Text(
+    'Create an account',
+    style: TextStyle(
+    fontSize: 16,
+    // Constructor Call #3
+    color: Colors.white,
+    fontWeight: FontWeight.w700),
+    ),
+    ),
+
+    ////////// Styling //////////
+    style: ButtonStyle(
+    splashFactory: NoSplash.splashFactory,
+    // Constructor Call #4
+    backgroundColor: MaterialStateProperty.all(Color(0xffb23fff)),
+    shape: MaterialStateProperty.all(RoundedRectangleBorder(
+    borderRadius: BorderRadius.circular(15),
+    ))),
+    ),
+                SizedBox(
+                  height: 50,
+                ),
+              ],
+            ),
+          ),
     );
   }
 }
--- lib/Screens/FirstPage.dart
+++ lib/Screens/FirstPage.dart
@@ -1,22 +1,88 @@
 import 'package:flutter/material.dart';
+import 'package:simple_gradient_text/simple_gradient_text.dart';
+
 
 class FirstPage extends StatefulWidget {
   const FirstPage({Key? key}) : super(key: key);
 
   @override
-  State<FirstPage> createState() => _FirstPageState();
+  _FirstPageState createState() => _FirstPageState();
 }
 
 class _FirstPageState extends State<FirstPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('First Page',
-            style: TextStyle(
-              fontSize: 20,
-              color: Colors.grey,
-            )),
+      body: Column(
+        children: [
+          Stack(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                        begin: FractionalOffset.topLeft,
+                        end: FractionalOffset.bottomRight,
+                        colors: [Color(0xffD48EFF),Color(0xff482ABF)]
+
+                    ),
+                    borderRadius: BorderRadius.only(bottomLeft: Radius.circular(50),bottomRight: Radius.circular(50))),
+                width: double.infinity,
+                height: 500,
+              ),
+              Column(
+                children: [
+                  SizedBox(
+                    height: 100,
+                  ),
+                  Center(
+                    child: Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Image.asset('assets/images/SignIn.png'),
+                    ),
+                  ),
+                ],
+              )
+
+            ],),
+          SizedBox(
+            height: 50,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Listen to your',
+                  style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w400
+                  ),),
+
+                Text('Favorite Podcast',
+                  style: TextStyle(
+                      fontSize: 35,
+                      fontWeight: FontWeight.w800
+                  ),),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text('with ',
+                      style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w400
+                      ),),
+                    GradientText('AirCast', colors: [Color(0xffD48EFF),Color(0xff482ABF)],
+                      style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w800
+                      ),),
+                  ],
+                ),
+              ], ),
+          ),
+
+        ],
       ),
     );
   }
